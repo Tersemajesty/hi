ac45e19158b85199369e53f9bc2dc64c48636f13
Tersemajesty <okekechidera992@gmail.com>
2025-02-17 09:06:16 -0800

    ggg

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -1,11 +1,11 @@
 import { ScaleLoader } from 'react-spinners'
-// import style from "./Loader.module.css"
+import style from "./Loader.module.css"
 
 
 const Loader = () => {
     return (
       <div>
-        <span className="Loader"></span>
+        <span className={style.Loader}></span>
         <ScaleLoader />
       </div>
     );
