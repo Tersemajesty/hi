cd79373eef9c8f5a6e12e8e55dace2c7402fcafa
terse <okekechidera992@gmail.com>
2025-05-15 14:55:28 +0100

    changs

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -5,6 +5,10 @@ import style from "./pages.module.css"
     return (
         <div className={style.ProfilePage}>
             <div className={style.ProfilePage__container}>
+                <h1 className={style.ProfilePage__title}>Profile</h1>
+                <div className={style.ProfilePage__content}>
+                    <p className={style.ProfilePage__text}>This is the profile page.</p>
+                </div>
 
             </div>
         </div>
